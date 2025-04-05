73f0b92f4a627401b3081465e15753e7dd52b2a0
Alumno <Alumno@D302-010>
2025-04-05 14:01:20 -0500

    Agregar archivos de proyecto.

---
+++ CRUD-MVC/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_MVC.Models
+{
+    public class Product
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Descripcion { get; set; }
+        public int IdCategoria { get; set; }
+
+
+
+
+
+    }
+}
